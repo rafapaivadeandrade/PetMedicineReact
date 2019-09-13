26e62e9db1f5ceaa705706d1c6cafd276432d918
Rafael <rafaklose>
2019-09-13 01:03:31 -0700

    Updates

---
+++ precache-manifest.4f47d5477add4e6a34167bea8717e5d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ac8c722807720538f4ddcce6d13650",
+    "url": "/PetMedicineReact/index.html"
+  },
+  {
+    "revision": "6683336d49b609339cc8",
+    "url": "/PetMedicineReact/static/css/main.70bc7e0f.chunk.css"
+  },
+  {
+    "revision": "fe2ab3271d956818a0ed",
+    "url": "/PetMedicineReact/static/js/2.8b1b313c.chunk.js"
+  },
+  {
+    "revision": "6683336d49b609339cc8",
+    "url": "/PetMedicineReact/static/js/main.20d73007.chunk.js"
+  },
+  {
+    "revision": "235fac720b813f7ae3fe",
+    "url": "/PetMedicineReact/static/js/runtime~main.0f907c76.js"
+  }
+]);
